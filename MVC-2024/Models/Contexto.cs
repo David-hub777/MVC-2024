2f8aa09aac8b967aaac187ec12a562b96d355fba
David <david25042001@gmail.com>
2023-12-18 12:45:39 +0100

    Commit de prueba

---
+++ MVC-2024/Models/Contexto.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_2024.Models
+{
+    public class Contexto : DbContext
+    {
+    }
+}
---
+++ MVC-2024/Models/marcaModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC_2024.Models
+{
+    public class marcaModel
+    {
+        public int Id { get; set; }
+        public string NomMarca { get; set; } //= string.Empty;
+    }
+}
