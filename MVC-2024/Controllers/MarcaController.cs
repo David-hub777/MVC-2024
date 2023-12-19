e5718474f0f31617afc22861badc58dee38af31e
David <david25042001@gmail.com>
2023-12-19 13:35:29 +0100

    Mas estable pero falta Delete

--- MVC-2024/Controllers/MarcaController.cs
+++ MVC-2024/Controllers/MarcaController.cs
@@ -70,22 +70,86 @@ namespace MVC_2024.Controllers
         // GET: MarcaController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            // Busca el elemento directamente en la base de datos usando el id proporcionado
+            MarcaModel elemento = this.Contexto.Marcas.Find(id);
+
+            // Verifica si se encontró el elemento
+            if (elemento != null)
+            {
+                // Si se encontró, pasa el elemento correspondiente a la vista
+                return View(elemento);
+            }
+            else
+            {
+                // Si no se encontró, puedes manejarlo de la manera que consideres apropiada
+                // Por ejemplo, puedes redirigir a una página de error.
+                return RedirectToAction("Error");
+            }
+            //List<MarcaModel> lista = this.Contexto.Marcas.ToList();
+            //// Encuentra el índice del elemento con el ID especificado
+            //int indice = lista.FindIndex(item => item.Id == id);
+
+            //// Verifica si se encontró el elemento
+            //if (indice != -1)
+            //{
+            //    // Si se encontró, pasa el elemento correspondiente a la vista
+            //    return View(lista[indice]);
+            //}
+            //else
+            //{
+            //    // Si no se encontró, puedes manejarlo de la manera que consideres apropiada
+            //    // Por ejemplo, puedes redirigir a una página de error.
+            //    return RedirectToAction("Error");
+            //}
+            //return View();
         }
 
         // POST: MarcaController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, IFormCollection collection,MarcaModel marca)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                // Buscar el elemento en la base de datos por el ID
+                MarcaModel elemento = Contexto.Marcas.Find(id);
+
+                if (elemento != null)
+                {
+                    // Actualizar propiedades del elemento con los valores proporcionados por el modelo
+                    elemento.Id = marca.Id; // Reemplaza Propiedad1 con el nombre real de tus propiedades
+                    elemento.NomMarca = marca.NomMarca; // Reemplaza Propiedad2 con el nombre real de tus propiedades
+                                                            // ...
+
+                    // Guardar los cambios en la base de datos
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    // Si el elemento no se encuentra, puedes manejarlo como consideres apropiado
+                    return RedirectToAction("Error");
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                // Puedes manejar la excepción de manera adecuada para tu aplicación
+                // Por ejemplo, puedes agregar registros de errores, mostrar un mensaje al usuario, etc.
+                return RedirectToAction("Error");
             }
+            //MarcaModel elemento = Contexto.Marcas.Find(id);
+            //// como cambio elemento por marca?
+            //Contexto.Database.EnsureCreated();
+            //Contexto.SaveChanges();
+            //try
+            //{
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
         }
 
         // GET: MarcaController/Delete/5
