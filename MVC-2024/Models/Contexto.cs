eb724d90974866c0b1135c9ff26be0c5b69804cf
David <david25042001@gmail.com>
2023-12-19 11:33:02 +0100

    Empieza a ser estable

--- MVC-2024/Models/Contexto.cs
+++ MVC-2024/Models/Contexto.cs
@@ -7,6 +7,7 @@ namespace MVC_2024.Models
         public DbSet<marcaModel> Marcas { get; set; }//Debe coincidir con la TABLA de la DB
         public Contexto(DbContextOptions<Contexto> options) : base(options)
         {//Crea la DB Automatic
+
         }
     }
 }
